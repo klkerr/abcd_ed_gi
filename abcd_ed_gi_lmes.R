c12e11f34f3f30e370b044ddcec8eeb8fd58e49f
klkerr <44510268+klkerr@users.noreply.github.com>
2020-12-18 10:29:24 -0600

    Removed unnecessary covariates
    Also updated correlation matrix code to include all available data

--- abcd_ed_gi_lmes.R
+++ abcd_ed_gi_lmes.R
@@ -20,7 +20,7 @@ sink("abcd_ed_gi_gam_output.txt",split=TRUE)
 ###########  Regression  ###############
 ########################################
 
-const.vars=c("subjectid","abcd_site.x","rel_family_id.x","b.bmi","race_ethnicity.x","gender","high.educ.x","married.or.livingtogether.x","y1.interview_age","household.income.x")
+const.vars=c("subjectid","abcd_site.x","rel_family_id.x","race_ethnicity.x","gender","married.or.livingtogether.x","household.income.x")
 
 #Center interaction terms
 abcd.ed.gi$b.gi_sum.centered = scale(abcd.ed.gi$b.gi_sum,center=TRUE,scale=FALSE)
@@ -36,43 +36,54 @@ abcd.ed.gi=abcd.ed.gi[abcd.ed.gi$gender=="F" | abcd.ed.gi$gender=="M",]
 abcd.ed.gi.m_crpbi = abcd.ed.gi.m_crpbi[abcd.ed.gi.m_crpbi$gender=="F" | abcd.ed.gi.m_crpbi$gender=="M",]
 abcd.ed.gi.f_crpbi = abcd.ed.gi.f_crpbi[abcd.ed.gi.f_crpbi$gender=="F" | abcd.ed.gi.f_crpbi$gender=="M",]
 
-#Make correlation matrices
-cont.vars=c("b.bmi","y1.interview_age","b.gi_sum")
-abcd.base=left_join(abcd.ed.gi,abcd.ed.gi.m_crpbi,by="subjectid")
-abcd.base=left_join(abcd.base,abcd.ed.gi.f_crpbi,by="subjectid")
-
 writeLines("#########################################")
 writeLines("Whole sample correlation matrix (Table 3)")
 writeLines("#########################################")
 
-data=abcd.base[c(cont.vars,"b.cbcl_scr_dsm5_anxdisord_t","b.crpbi_mother","b.crpbi_father")]
-cor(data,method="pearson",use="pairwise.complete.obs")
+#Make correlation matrices
+cont.vars=c("b.bmi","y1.interview_age","b.gi_sum")
+
+data=abcd.ed.gi[c(cont.vars,"b.cbcl_scr_dsm5_anxdisord_t")]
+corr.test(data, adjust = "none", use = "pairwise")
+
+data=abcd.ed.gi.m_crpbi[c(cont.vars,"b.cbcl_scr_dsm5_anxdisord_t","b.crpbi_mother")]
+corr.test(data, adjust = "none", use = "pairwise")
+
+data=abcd.ed.gi.f_crpbi[c(cont.vars,"b.cbcl_scr_dsm5_anxdisord_t","b.crpbi_father")]
+corr.test(data, adjust = "none", use = "pairwise")
+
+abcd.m.temp = abcd.ed.gi.m_crpbi[c("subjectid","b.crpbi_mother")]
+abcd.m.temp=abcd.m.temp[complete.cases(abcd.m.temp),]
+abcd.f.temp = abcd.ed.gi.f_crpbi[c("subjectid","b.crpbi_father")]
+abcd.f.temp=abcd.f.temp[complete.cases(abcd.f.temp),]
+abcd.m.f.temp = merge(abcd.m.temp,abcd.f.temp,by="subjectid")
+cor.test(abcd.m.f.temp$b.crpbi_mother,abcd.m.f.temp$b.crpbi_father,use="pairwise.complete.obs",method="pearson")
 
 writeLines("###########################################")
-writeLines("Paternal Acceptance LME (Table 4; Table S1)")
+writeLines("Paternal Acceptance LME (Table 4; Table S3)")
 writeLines("###########################################")
 
 #Paternal acceptance
-data=abcd.ed.gi.f_crpbi[c(const.vars,"b.ed_sum","b.gi_sum.centered","y1.ed_sum","y1.interview_age","b.crpbi_father.centered","b.cbcl_scr_dsm5_anxdisord_t")]
+data=abcd.ed.gi.f_crpbi[c(const.vars,"b.ed_sum","b.gi_sum.centered","y1.ed_sum","b.crpbi_father.centered","b.cbcl_scr_dsm5_anxdisord_t")]
 data=data[complete.cases(data),]
 
-y1.ed_pred_b.gi_b.fcrpbi=lmer(y1.ed_sum ~ b.ed_sum + b.gi_sum.centered + b.bmi + race_ethnicity.x + gender + high.educ.x + married.or.livingtogether.x + y1.interview_age + household.income.x + b.cbcl_scr_dsm5_anxdisord_t + b.crpbi_father.centered +
-			gender * b.gi_sum.centered + b.crpbi_father.centered * b.gi_sum.centered + gender * b.crpbi_father.centered * b.gi_sum.centered + (1|abcd_site.x/rel_family_id.x),data=data)
+y1.ed_pred_b.gi_b.fcrpbi=lmer(y1.ed_sum ~ b.ed_sum + b.gi_sum.centered + race_ethnicity.x + gender + married.or.livingtogether.x + household.income.x + b.cbcl_scr_dsm5_anxdisord_t + b.crpbi_father.centered +
+				gender * b.crpbi_father.centered * b.gi_sum.centered + (1|abcd_site.x/rel_family_id.x),data=data)
 
 summary(y1.ed_pred_b.gi_b.fcrpbi)
 r.squaredGLMM(y1.ed_pred_b.gi_b.fcrpbi)
 standardize(y1.ed_pred_b.gi_b.fcrpbi)
 
-#Maternal acceptance
-data=abcd.ed.gi.m_crpbi[c(const.vars,"b.ed_sum","b.gi_sum.centered","y1.ed_sum","y1.interview_age","b.crpbi_mother.centered","b.gi_sum","b.crpbi_mother","b.cbcl_scr_dsm5_anxdisord_t")]
-data=data[complete.cases(data),]
-
-y1.ed_pred_b.gi_b.mcrpbi=lmer(y1.ed_sum ~ b.ed_sum + b.gi_sum.centered + b.bmi + race_ethnicity.x + gender + high.educ.x + married.or.livingtogether.x + y1.interview_age + household.income.x + b.cbcl_scr_dsm5_anxdisord_t + b.crpbi_mother.centered +
-			gender * b.gi_sum.centered + b.crpbi_mother.centered * b.gi_sum.centered + gender * b.crpbi_mother.centered * b.gi_sum.centered + (1|abcd_site.x/rel_family_id.x),data=data)
-
 writeLines("###########################################")
 writeLines("Maternal Acceptance LME (Table 5; Table S2)")
 writeLines("###########################################")
+
+#Maternal acceptance
+data=abcd.ed.gi.m_crpbi[c(const.vars,"b.ed_sum","b.gi_sum.centered","y1.ed_sum","b.crpbi_mother.centered","b.cbcl_scr_dsm5_anxdisord_t")]
+data=data[complete.cases(data),]
+
+y1.ed_pred_b.gi_b.mcrpbi=lmer(y1.ed_sum ~ b.ed_sum + b.gi_sum.centered + race_ethnicity.x + gender + married.or.livingtogether.x + household.income.x + b.cbcl_scr_dsm5_anxdisord_t + b.crpbi_mother.centered +
+				gender * b.crpbi_mother.centered * b.gi_sum.centered + (1|abcd_site.x/rel_family_id.x),data=data)
 
 writeLines("Results for GI symptoms and maternal acceptance:")	
 summary(y1.ed_pred_b.gi_b.mcrpbi)
@@ -83,16 +94,16 @@ writeLines("##################################################")
 writeLines("Maternal acceptance LME results by participant sex")
 writeLines("##################################################")
 writeLines("\n##################")
-writeLines("Females (Table S3)")
+writeLines("Females (Table S5)")
 writeLines("##################")
 
 girls.abcd.ed.gi.m_crpbi=abcd.ed.gi.m_crpbi[abcd.ed.gi.m_crpbi$gender=="F",]
 boys.abcd.ed.gi.m_crpbi=abcd.ed.gi.m_crpbi[abcd.ed.gi.m_crpbi$gender=="M",]
 
-data=girls.abcd.ed.gi.m_crpbi[c(const.vars,"b.ed_sum","b.gi_sum.centered","y1.ed_sum","y1.interview_age","b.crpbi_mother.centered","b.gi_sum","b.crpbi_mother","b.cbcl_scr_dsm5_anxdisord_t")]
+data=girls.abcd.ed.gi.m_crpbi[c(const.vars,"b.ed_sum","b.gi_sum.centered","y1.ed_sum","b.crpbi_mother.centered","b.cbcl_scr_dsm5_anxdisord_t")]
 data=data[complete.cases(data),]
 
-g.y1.ed_pred_b.gi_b.mcrpbi=lmer(y1.ed_sum ~ b.ed_sum + b.gi_sum.centered + b.bmi + race_ethnicity.x + high.educ.x + married.or.livingtogether.x + y1.interview_age + household.income.x + b.cbcl_scr_dsm5_anxdisord_t + b.crpbi_mother.centered +
+g.y1.ed_pred_b.gi_b.mcrpbi=lmer(y1.ed_sum ~ b.ed_sum + b.gi_sum.centered + race_ethnicity.x + married.or.livingtogether.x + household.income.x + b.cbcl_scr_dsm5_anxdisord_t + b.crpbi_mother.centered +
 			b.crpbi_mother.centered * b.gi_sum.centered + (1|abcd_site.x/rel_family_id.x),data=data)
 
 summary(g.y1.ed_pred_b.gi_b.mcrpbi)
@@ -100,13 +111,13 @@ r.squaredGLMM(g.y1.ed_pred_b.gi_b.mcrpbi)
 standardize(g.y1.ed_pred_b.gi_b.mcrpbi)
 
 writeLines("##################")
-writeLines("Males (Table S4)")
+writeLines("Males (Table S6)")
 writeLines("##################")
 
-data=boys.abcd.ed.gi.m_crpbi[c(const.vars,"b.ed_sum","b.gi_sum.centered","y1.ed_sum","y1.interview_age","b.crpbi_mother.centered","b.gi_sum","b.crpbi_mother","b.cbcl_scr_dsm5_anxdisord_t")]
+data=boys.abcd.ed.gi.m_crpbi[c(const.vars,"b.ed_sum","b.gi_sum.centered","y1.ed_sum","b.crpbi_mother.centered","b.cbcl_scr_dsm5_anxdisord_t")]
 data=data[complete.cases(data),]
 
-b.y1.ed_pred_b.gi_b.mcrpbi=lmer(y1.ed_sum ~ b.ed_sum + b.gi_sum.centered + b.bmi + race_ethnicity.x + high.educ.x + married.or.livingtogether.x + y1.interview_age + household.income.x + b.cbcl_scr_dsm5_anxdisord_t + b.crpbi_mother.centered +
+b.y1.ed_pred_b.gi_b.mcrpbi=lmer(y1.ed_sum ~ b.ed_sum + b.gi_sum.centered + race_ethnicity.x + married.or.livingtogether.x + household.income.x + b.cbcl_scr_dsm5_anxdisord_t + b.crpbi_mother.centered +
 			b.crpbi_mother.centered * b.gi_sum.centered + (1|abcd_site.x/rel_family_id.x),data=data)
 
 summary(b.y1.ed_pred_b.gi_b.mcrpbi)
